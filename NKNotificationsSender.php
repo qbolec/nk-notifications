b22eac61e9bb0e3358edb944d5d54050919e593f
Jakub Łopuszański <jakub.lopuszanski@nasza-klasa.pl>
2014-05-12 16:00:40 +0200

    aha

---
+++ NKNotificationsSender.php
@@ -0,0 +1,42 @@
+<?php
+class NKException extends Exception{}
+class NKNotificationsSender{
+  private $endpoint_url = 'http://opensocial.nk-net.pl/v09/social/rest/messages/@me/@self/@outbox';
+  private $app_id = 273;
+  private $key = 'dupa biskupa';
+  private $secret = '8a6eafc1-9971-4559-b7aa-bb1b558068c5';
+  private $consumer;
+
+  public function __construct(){
+    $this->consumer = new OAuthConsumer($this->key, $this->secret);
+  }
+
+  public function send($body,$link_title,array $uri_params,array $recipients_ids){
+    $jsoned_data = json_encode(array(
+      'title' => $link_title,
+      'body' => $body,
+      'type' => 'notification',
+      'recipients' => $recipients_ids,
+      'nkOptUrlParams' => $uri_params,
+    ));
+    $body_hash = base64_encode(sha1($jsoned_data, true));
+
+    $request = OAuthRequest::from_consumer_and_token($this->consumer, null, 'POST', $this->endpoint_url, array('oauth_body_hash' => $body_hash));
+    $request->sign_request(new OAuthSignatureMethod_HMAC_SHA1(), $this->consumer, null);
+
+    $handle = curl_init($request->to_url());
+    curl_setopt($handle, CURLOPT_POST, true);
+    curl_setopt($handle, CURLOPT_POSTFIELDS, $jsoned_data);
+    curl_setopt($handle, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($handle); 
+    if($response === false) {
+      throw new NKException('network error making HTTP request');
+    }
+    $code = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+    if($code !== 200) {
+      throw new NKException("HTTP error $code, body: $response");
+    }
+  }
+
+};
---
+++ send_notifications.php
@@ -0,0 +1,38 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors','stderr');
+
+if(!function_exists('curl_init')) {
+  die("curl extension not available\n");
+}
+
+require 'OAuth.php';
+require 'NKNotificationsSender.php';
+
+
+$options = getopt(null,array(
+  'uri_params:',
+  'body:',
+  'link_title:',
+));
+$uri_params = array();
+parse_str($options['uri_params'],$uri_params);
+$body = $options['body'];
+$link_title = $options['link_title'];
+
+$sender = new NKNotificationsSender();
+$recipients_ids = array();
+while(!feof(STDIN)){
+  $person_id = trim(fgets(STDIN));
+  if(!$person_id){
+    break;
+  }
+  $recipients_ids[] = $person_id;
+  if(count($recipients_ids) == 100){ //you can send the same notification to at most 100 users with single call
+    $sender->send($body,$link_title,$uri_params,$recipients_ids);
+    $recipients_ids = array();
+  }
+}
+if(!empty($recipients_ids)){
+  $sender->send($body,$link_title,$uri_params,$recipients_ids);
+}
